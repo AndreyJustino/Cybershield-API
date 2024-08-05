64ec2dff2a6dac567a98a8ebec70a49ad86e3a14
AndreyJustino <andreyjustino2020@gmail.com>
2024-08-05 18:05:44 -0300

    configurando e conectando api ao banco

---
+++ src/model/model.js
@@ -0,0 +1,24 @@
+import sequelize from "../database/config.js";
+import { DataTypes } from "sequelize";
+
+// criando tabela usuario
+export const Cybershield = sequelize.define("Usuario", {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+})
