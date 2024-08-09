d60a181facc83de1e76c0a246fe86c2ddcdc7031
gafanhoto23 <i.oliveiraa02@gmail.com>
2024-08-09 18:21:08 -0300

    Feat: Finalizado CRUD

--- src/routes/reductionsActions.routes.js
+++ src/routes/reductionsActions.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { PostReductionsActions } from "../controller/reductionsActions/postReductionsactions.js";
 import { getReductionsActions } from "../controller/reductionsActions/getReductionsActions.js";
+import { DeleteReductionAction } from "../controller/reductionsActions/deleteReductionAction.js";
+import { UpReductionAction } from "../controller/reductionsActions/updateReductionAction.js";
 
 const routesReductionsActions = Router()
+    routesReductionsActions.post("/postReductionsactions", PostReductionsActions)
     routesReductionsActions.get("/reductionsActions", getReductionsActions)
-
+    routesReductionsActions.delete("/deleteReductionAction", DeleteReductionAction)
+    routesReductionsActions.put("/updateReductionAction", UpReductionAction)
 export default routesReductionsActions
