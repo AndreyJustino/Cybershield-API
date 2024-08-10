8fe898550b6d1f1111fbd5a732088b49c988b544
AndreyJustino <andreyjustino2020@gmail.com>
2024-08-10 18:04:22 -0300

    teste unitario da deleteUser

---
+++ src/controller/user/deleteUser.test.js
@@ -0,0 +1,56 @@
+const request = require("supertest")
+require("dotenv").config()
+
+describe("Testing response deleteUser", () => {
+    test("Should return status 400 if there are empty fields", async () => {
+        const response = await request(`http://localhost:${process.env.PORT_API}`)
+            .delete("/deleteUser")
+            .send({
+                "email": "",
+                "password": "senha123"
+            })
+        expect(response.status).toBe(400)
+        expect(response.body).toStrictEqual({"message": "Preencha todos os campos."})
+    })
+
+    test("Should return status 404 if user don't found", async () => {
+        const response = await request(`http://localhost:${process.env.PORT_API}`)
+            .delete("/deleteUser")
+            .send({
+                "email": "email@email.com",
+                "password": "senha123"
+            })
+        expect(response.status).toBe(404)
+        expect(response.body).toStrictEqual({"message": "Usuário não encontrado."})
+    })
+
+    test("Should return status 401 if password is incorrect", async () => {
+        await request(`http://localhost:${process.env.PORT_API}`)
+            .post('/register')
+            .send({
+                "name": "delete",
+                "password": "delete123",
+                "email": "delete@email.com",
+            })
+        
+        const response = await request(`http://localhost:${process.env.PORT_API}`)
+            .delete('/deleteUser')
+            .send({
+                "email": "delete@email.com",
+                "password": "delete1234"
+            })
+        expect(response.status).toBe(401)
+        expect(response.body).toStrictEqual({"message": "Senha incorreta."})
+    })
+
+    test("Should return status 200 if user delete", async () => {
+        const response = await request(`http://localhost:${process.env.PORT_API}`)
+            .delete("/deleteUser")
+            .send({
+                "email": "delete@email.com",
+                "password": "delete123"
+            })
+            expect(response.status).toBe(200)
+            expect(response.body).toStrictEqual({"message": "Usuário deletado com sucesso."})
+    })
+})
