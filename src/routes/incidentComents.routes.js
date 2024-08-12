9f967f3eb8ccf130371990704aaf3fe49cbc8454
Andre Nascimento <1914andre@gmail.com>
2024-08-12 13:12:09 -0300

    Feat: criando delete

--- src/routes/incidentComents.routes.js
+++ src/routes/incidentComents.routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
-import { CreateComent } from "../controller/incident_coments/post_IncidentComents.controler.js";
-import { GetIncidentComents } from "../controller/incident_coments/get_IncidentComents.controler.js";
+import { CreateComent } from "../controller/incident_coments/post_IncidentComents.controller.js";
+import { GetIncidentComents } from "../controller/incident_coments/get_IncidentComents.controller.js";
+import { generateUpdate } from "../controller/incident_coments/update_IncidentComents.controller.js";
+import { deleteIncidentComent } from "../controller/incident_coments/delete_IncidentComents.controller.js";
 
 
 const routesComent = Router()
 
 routesComent.post("newIncidentComent",CreateComent)
-routesComent.get("AllIncidentComent", GetIncidentComents)
+routesComent.get("AllIncidentsComents",GetIncidentComents)
+routesComent.put("UpIncidentsComents",generateUpdate)
+routesComent.delete("DeleteIncidentComents",deleteIncidentComent)
 
 
 export default routesComent
