eb823592977a0e4cfbb959e789d7e965e6c48050
Andre Nascimento <1914andre@gmail.com>
2024-08-13 21:29:51 -0300

    feat:criando test Controller: Post IncidentComent

---
+++ src/controller/incident_coments/post.IncidentComent.test.js
@@ -0,0 +1,34 @@
+import app from "../../app.js";
+import { incidentComent } from "../../model/model.js";
+import request from "supertest";
+
+jest.mock("../../model/model.js") 
+
+describe("testando controller:PostIncidentType",() =>{
+    it("deve criar um novo incident Type",async () =>{
+        const MyMock = [{
+            id:1,name:"neymar",gmail:"neyma@gmail.com",
+            id:2,name:"suarez",gmail:"susu@gmail.com",
+            id:3,name:"coutinho",gmail:"coutao@gmail.com",
+        }]
+        
+        incidentComent.create.mockResolveValue(MyMock)
+        const response = await request(app).post("/newIncidentComent")
+       
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(MyMock)
+    })
+})
+
+describe("testando controller:PostIncidentType",()=>{
+    it("deve criar um novo incident Type", async()=>{
+        
+        incidentComent.create.mockRejectValue(new Error("Error"))
+        const response = await request(app).post("/newIncidentComent")
+       
+        expect(response.status).toBe(500)
+        expect(response.body).toEqual({
+            error:"erro ao criar novo comentario de incidente"
+        })
+    })
+})
